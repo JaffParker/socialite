7f3a66cf435b766317c37539b9bc4fb9e8ab4dc4
Andrey Kamozin <andreykamozin@gmail.com>
2016-04-19 13:17:03 -0400

    Updated Medium provider with token fix.

--- src/Two/GoogleProvider.php
+++ src/Two/GoogleProvider.php
@@ -89,7 +89,7 @@ class GoogleProvider extends AbstractProvider implements ProviderInterface
             ],
             'headers' => [
                 'Accept' => 'application/json',
-                'Authorization' => 'Bearer '.$token,
+                'Authorization' => 'Bearer '.$token['access_token'],
             ],
         ]);
 
